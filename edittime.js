e8860e39ae0af283fe78c3235a8642714416765c
AZARI Nicolas <nicodinh@azari.fr>
2015-01-19 00:38:32 +0100

    update help url

--- edittime.js
+++ edittime.js
@@ -6,7 +6,7 @@
 		"version":		"0.1",					// (float in x.y format) Plugin version - C2 shows compatibility warnings based on this
 		"description":	"youtube plugin for Construct 2",
 		"author":		"AZARI Nicolas",
-		"help url":		"http://nicolas.azari.fr/construct2/plugins/youtubec2",
+		"help url":		"https://github.com/nicodinh/youtubec2",
 		"category":		"Platform specific",	// Prefer to re-use existing categories, but you can set anything here
 		"type":			"world",				// either "world" (appears in layout and is drawn), else "object"
 		"rotatable":	false,					// only used when "type" is "world".  Enables an angle property on the object.
